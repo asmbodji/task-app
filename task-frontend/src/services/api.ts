73ec6694de81c161f5cd021e790813607f687c7f
Amadou Sow Mbodji <asmbodj@hotmail.com>
2025-07-24 13:20:00 +0200

    use env for API URL

--- task-frontend/src/services/api.ts
+++ task-frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/tasks";
+const API_URL = process.env.REACT_APP_API_URL!;
 
 export const getTasks = async () => {
   const response = await axios.get(API_URL);
